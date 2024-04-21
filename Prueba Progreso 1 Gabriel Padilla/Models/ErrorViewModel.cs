0ea5475bf016f9969dace2fa33a097c66a6300e0
usuario <usuario@ADMIN>
2024-04-21 10:24:54 -0500

    Agregar archivos de proyecto.

---
+++ Prueba Progreso 1 Gabriel Padilla/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prueba_Progreso_1_Gabriel_Padilla.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
